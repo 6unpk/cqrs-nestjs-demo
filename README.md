8e6e6fd884004e6f16c8ba9432dc92d1eb712a2b
Junu Park <pkjoho95@gmail.com>
2022-05-30 16:51:30 +0900

    [Fix] bash script and readme

--- README.md
+++ README.md
@@ -18,14 +18,16 @@ yarn install
 
 MacOs, Linux 의 경우 다음 sh 스크립트를 실행합니다. DB 및 관련 의존성 설정을 자동으로 진행합니다. 별도의 인자를 넣지 않으면 기본 설정값으로 진행됩니다.
 
+```-p [포트] -u [기본사용자] -P [기본사용자 패스워드]```
+
 ```
-./setup -port 3306 -username <db_username> -password <db_password>
+./setup.sh
 ```
 
 마찬가지로 Windows 의 경우 Powershell 스크립트를 실행합니다.
 
 ```
-./setup.ps1 -port 3306 -username <db_username> -password <db_password>
+./setup.ps1
 ```
 
 ### Test
--- setup.sh
+++ setup.sh
@@ -1,38 +1,47 @@
+#!/bin/bash
+
+help() {
+  echo "READ README.MD!"
+}
+
+while getopts "p:u:P:h" opt
+do
+  case $opt in
+    p) arg_p=$OPTARG
+      ;;
+    u) arg_u=$OPTARG
+      ;;
+    P) arg_P=$OPTARG
+      ;;
+    h) help ;;
+  esac
+done
+
 # Setup Env
-if [[ -z "$1" ]]; then
-  echo "No database port given, use default port"
-  $port = 3306
-fi
-
-if [[ -z "$2" ]]; then
-  echo "No database username given, use default username"
-  $username = 'user'
-fi
-
-if [[ -z "$2" ]]; then
-  echo "No database password given, use default port"
-  $password = 'abcd'
+if [[ -z "$arg_p" ]]; then
+  echo "use default port"
+  port=3306
+else
+  port="$arg_p"
 fi
 
 
-# Check liquibase
-if ! command -v liquibase &> /dev/null
-then
-    echo "liquibase could not be found"
-    echo "liquibase가 설치되지 않았거나 PATH에 등록되지 않았습니다!"
-    echo "liquibase 설치를 진행하겠습니까? [Y\N]"
-    read keep
-    if [[ -z "$1" ]]; then
-      echo "No database port given, use default port"
-    fi
-    else
-      curl http://sourceforge.net/projects/liquibase/files/Liquibase%20Core/liquibase-3.2.0-bin.tar.gz/download -O liquibase-3.2.0-bin.tar.gz
-      tar -xvzf liquibase-3.2.0-bin.tar.gz
+if [[ -z "$arg_u" ]]; then
+  echo "use default username"
+  username="user"
+else
+  username="$arg_u"
+fi
 
+if [[ -z "$arg_P" ]]; then
+  echo "use default password"
+  password="abcd"
+else
+  password="$arg_P"
 fi
 
 
-# Check Docker & Docker Compose
+# Check Docker & Docker Compose & Yarn
 if ! command -v docker &> /dev/null
 then
     echo "docker could not be found"
@@ -48,13 +57,19 @@ then
     exit
 fi
 
-# Run docker-compose
-docker-compose up 
+if ! command -v yarn &> /dev/null
+then
+    echo "yarn could not be found"
+    echo "Yarn이 설치되지 않았거나 PATH에 등록되지 않았습니다!"
+    exit
+fi
 
-# Run Liquibase
-./liquibase \
-    --url="localhost:$port" \
-    --username="$username" \
-    --password="$password" \
-    --defaults-file="migrations/liquibase.properties" \
-    update
+
+# Run Yarn install
+yarn
+
+sed -i -e "s/user/$username/" -e "s/abcd/$password/" -e "s/127.0.0.1:3306/127.0.0.1:$port/" docker-compose.yaml
+sed -i -e "s/user/$username/" -e "s/abcd/$password/" -e "s/3306/$port/" .env
+
+# Run docker-compose
+docker-compose up
